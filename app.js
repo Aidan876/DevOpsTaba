38046c3df0af205757f83c9455eb4e43c12fc263
Your Name <my@email.org>
2024-04-12 15:45:54 +0100

    html page edits

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var contactRouter = require('./routes/contact');
+var indexRouter = require('./routes/index', './routes/contact');
 
 var app = express();
 
@@ -19,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter, contactRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
